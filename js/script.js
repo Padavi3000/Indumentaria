4f6238050cec4bad07220212e66eece7ae40af98
francoluvisotti <franco_luvi@hotmail.com>
2024-05-14 12:42:20 -0300

    agrego js y estilos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Variables
 const   carrito = document.getElementById('carrito'),
-        listaCursos = document.getElementById('lista-cursos'),
+        listaPrendas = document.getElementById('lista-prendas'),
         contenedorCarrito = document.querySelector('.buy-card .divisionProductos'),
         vaciarCarritoBtn = document.querySelector('#vaciar_carrito');
 
@@ -9,43 +9,43 @@ registrarEventsListeners();
 
 function registrarEventsListeners() {
     //al dar click agregar al carrito
-    listaCursos.addEventListener('click', agregarCurso);
+    listaPrendas.addEventListener('click', agregarPrenda);
 }
 
-function agregarCurso(e) {
+function agregarPrenda(e) {
     if (e.target.classList.contains("agregar-carrito")) {
-       const cursoSeleccionado = e.target.parentElement.parentElement;
-       leerInfo(cursoSeleccionado)
+       const prendaSeleccionada = e.target.parentElement.parentElement;
+       leerInfo(prendaSeleccionada)
     }
 }
 //leer el contenido del html al que le damos click e extrae la info
-function leerInfo(curso) {
-    //crear objeto con el contenido e la foto actuial
-    const infoCurso = {
-        imagen : curso.querySelector('img').src,
-        titulo : curso.querySelector('h3').textContent,
-        precio : curso.querySelector('.descuento').textContent,
-        id : curso.querySelector('button').getAttribute('data-id'),
+function leerInfo(prenda) {
+    //crear objeto con el contenido e la foto actual
+    const infoPrenda = {
+        imagen : prenda.querySelector('img').src,
+        titulo : prenda.querySelector('h3').textContent,
+        precio : prenda.querySelector('.descuento').textContent,
+        id : prenda.querySelector('button').getAttribute('data-id'),
         cantidad : 1
     }
-    // console.log(infoCurso)
+    // console.log(infoPrenda)
     //agregamos articulos al carrito
-    articulosCarrito = [...articulosCarrito, infoCurso]
+    articulosCarrito = [...articulosCarrito, infoPrenda]
     // console.log(articulosCarrito)
-    carritoHTML()
+    carritoHTML() //invoco la funcion cada vez que agrego item al array
 }
 
 //muestra el carrito en el html
 
 function carritoHTML() {
-    //recorre el carito y genra el html
-    articulosCarrito.forEach(curso => {
+    //recorre el carito y genera el html
+    articulosCarrito.forEach(prenda => {
         const fila = document.createElement('div');
         fila.innerHTML = `
-            <img src="${curso}"></img>
-            <p>${curso.titulo}</p>
-            <p>${curso.precio}</p>
-            <p>${curso.cantidad}</p>
+            <img src="${prenda}"></img>
+            <p>${prenda.titulo}</p>
+            <p>${prenda.precio}</p>
+            <p>${prenda.cantidad}</p>
             <p><span>X</span></p>
         `;
 
@@ -53,4 +53,11 @@ function carritoHTML() {
     });
 }
 
+//elimina las prendas de la lista_de_prendas
+function limpiarHTML(){
+    // console.log(contenedorCarrito.firstChild)
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
+}
 
