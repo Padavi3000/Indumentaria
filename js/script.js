bf5a948c4771378f7912b54f7da18ed42d87424f
francoluvisotti <franco_luvi@hotmail.com>
2024-05-15 10:48:55 -0300

    carrito terminado

--- js/script.js
+++ js/script.js
@@ -1,23 +1,49 @@
 // Variables
 const   carrito = document.getElementById('carrito'),
         listaPrendas = document.getElementById('lista-prendas'),
-        contenedorCarrito = document.querySelector('.buy-card .divisionProductos'),
+        contenedorCarrito = document.querySelector('.buy-card .lista_de_prendas'),
         vaciarCarritoBtn = document.querySelector('#vaciar_carrito');
 
 let articulosCarrito = [];
+
 registrarEventsListeners();
 
 function registrarEventsListeners() {
     //al dar click agregar al carrito
     listaPrendas.addEventListener('click', agregarPrenda);
+    //ELIMINAR CURSO DEL CARRITO
+    carrito.addEventListener('click', eliminarPrenda)
+    //MOSTRAR PRENDAS DEL CARRITO
+    //CON JSON.parse CONVIERTO A FORMATO ANTERIOR 
+    document.addEventListener("DOMContentLoaded", () => {
+        articulosCarrito = JSON.parse(localStorage.getItem("carrito")) || [];
+        carritoHTML()
+    })
+    //VACIAR EL CARRITO
+    vaciarCarritoBtn.addEventListener('click', e => {
+        articulosCarrito = [];
+        limpiarHTML()
+    })
+    //console.log(articulosCarrito)
 }
-
+//AGREGAR PRENDAS DEL CARRITO
 function agregarPrenda(e) {
     if (e.target.classList.contains("agregar-carrito")) {
        const prendaSeleccionada = e.target.parentElement.parentElement;
        leerInfo(prendaSeleccionada)
     }
 }
+
+//ELIMINAR PRENDAS DEL CARRITO
+function eliminarPrenda(e) {
+    if (e.target.classList.contains("borrar-prenda")) {
+        const prendaId = e.target.getAttribute('data-id');
+        //ELIMINAR DEL ARREGLO DE articulosCarrito POR EL data-id
+        articulosCarrito = articulosCarrito.filter(prenda => prenda.id !== prendaId)
+        carritoHTML()
+    }
+}
+
 //leer el contenido del html al que le damos click e extrae la info
 function leerInfo(prenda) {
     //crear objeto con el contenido e la foto actual
@@ -29,15 +55,32 @@ function leerInfo(prenda) {
         cantidad : 1
     }
     // console.log(infoPrenda)
-    //agregamos articulos al carrito
-    articulosCarrito = [...articulosCarrito, infoPrenda]
-    // console.log(articulosCarrito)
+    //Revisa si un elemento ya existe en el carrito
+    const existe = articulosCarrito.some(prenda => prenda.id === infoPrenda.id)
+    //console.log(existe)
+    if(existe) {
+        //ACTUALIZAR LA CANTIDAD DE PRENDAS
+        const prendas = articulosCarrito.map(prenda => {
+            if(prenda.id === infoPrenda.id) {
+                prenda.cantidad++;
+                return prenda
+            } else {
+                return prenda;
+            }
+        });
+        [...articulosCarrito,infoPrenda]
+    } else {
+        //agregamos articulos al carrito
+        articulosCarrito = [...articulosCarrito, infoPrenda]
+        //console.log(articulosCarrito)
+    }
     carritoHTML() //invoco la funcion cada vez que agrego item al array
 }
 
 //muestra el carrito en el html
 
 function carritoHTML() {
+    limpiarHTML()
     //recorre el carito y genera el html
     articulosCarrito.forEach(prenda => {
         const fila = document.createElement('div');
@@ -46,11 +89,16 @@ function carritoHTML() {
             <p>${prenda.titulo}</p>
             <p>${prenda.precio}</p>
             <p>${prenda.cantidad}</p>
-            <p><span>X</span></p>
+            <p><span class="borrar-prenda" data-id="${prenda.id}">X</span></p>
         `;
-
         contenedorCarrito.appendChild(fila)
     });
+    //Sincronizo con LocalStorage
+    sincronizarLocalStorage()
+}
+// CON JSON CONVIERTO A FORMATO PLANO EL CONTENIDO 
+function sincronizarLocalStorage() {
+    localStorage.setItem("carrito", JSON.stringify(articulosCarrito))
 }
 
 //elimina las prendas de la lista_de_prendas
@@ -59,5 +107,6 @@ function limpiarHTML(){
     while(contenedorCarrito.firstChild){
         contenedorCarrito.removeChild(contenedorCarrito.firstChild)
     }
+    sincronizarLocalStorage()
 }
 
